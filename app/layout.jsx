4432fd60a80eeef50e497614d3a437d0b6ce4e8f
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-26 23:04:33 +0300

    interFontFamily is added to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,14 @@
+import { Inter } from "@next/font/google"
+
 import "@/styles/reset.css"
 import "@/styles/global.css"
 
+
+const interFontFamily = Inter({ subsets: ["latin"] })
+
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={interFontFamily.className}>
 			<body className="container">{children}</body>
 		</html>
 	);
