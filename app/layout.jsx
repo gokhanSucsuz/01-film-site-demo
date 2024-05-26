a9a7208ff9f37145672ca459f4d7d54553dbb7e9
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-26 23:08:37 +0300

    layout is edited

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import { Inter } from "@next/font/google"
-
 import "@/styles/reset.css"
 import "@/styles/global.css"
+import Header from "@/components/header"
+import Footer from "@/components/footer"
 
 
 const interFontFamily = Inter({ subsets: ["latin"] })
@@ -9,7 +10,11 @@ const interFontFamily = Inter({ subsets: ["latin"] })
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" className={interFontFamily.className}>
-			<body className="container">{children}</body>
+			<body className="container">
+				<Header />
+				<main>{children}</main>
+				<Footer />
+			</body>
 		</html>
 	);
 }
