94ea8683a1bea39bd5d175fb2a979c54511e89d3
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-06-03 21:16:23 +0300

    popularMovies, topRatedMovies and categories are created from api

--- app/[[...category]]/page.jsx
+++ app/[[...category]]/page.jsx
@@ -1,23 +1,61 @@
 import HomeContainer from "@/containers";
 import Movies from "@/mocks/movies.json"
 
-async function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+const API_URL = "https://api.themoviedb.org/3/"
+
+
+const getCategories = async () => {
+    const res = await fetch(`${API_URL}/genre/movie/list?api_key=${process.env.API_KEY}&page=1`)
+    return res.json();
+
+}
+
+const getSingleCategory = async (genreId) => {
+    const res = await fetch(`${API_URL}/discover/movie?api_key=${process.env.API_KEY}&with_genres=${genreId}`)
+    return res.json();
+
+}
+
+const getTopRatedMovies = async () => {
+    const res = await fetch(`${API_URL}/movie/top_rated?api_key=${process.env.API_KEY}&page=1`)
+    return res.json();
+
+}
+
+const getPopularMovies = async () => {
+    const res = await fetch(`${API_URL}/movie/popular?api_key=${process.env.API_KEY}&page=1`)
+    return res.json();
+
 }
 
 export default async function Home({ params }) {
-    await delay(1000)
     let selectedCategory;
+
+    const topRatedPromise = getTopRatedMovies();
+    const popularPromise = getPopularMovies();
+    const categoriesPromise = getCategories();
+
+
+    const [{ results: topRatedMovies }, { results: popularMovies }, { genres: categories },] = await Promise.all([topRatedPromise, popularPromise, categoriesPromise])
+
+
     if (params?.category?.length > 0) {
-        selectedCategory = true
+        const { results } = await getSingleCategory(params.category[0])
+        selectedCategory = results;
     }
+
+    console.log(popularMovies)
 
     return (
         <>
-            <HomeContainer selectedCategory={{
-                id: params.category?.[0] ?? "",
-                movies: selectedCategory ? Movies.results.slice(0, 7) : []
-            }} />
+            <HomeContainer
+                topRatedMovies={topRatedMovies}
+                popularMovies={popularMovies}
+                categories={categories}
+                selectedCategory={{
+                    id: params.category?.[0] ?? "",
+                    movies: selectedCategory ? selectedCategory.slice(0, 7) : []
+                }} />
         </>
     );
 }
--- containers/index.jsx
+++ containers/index.jsx
@@ -1,28 +1,26 @@
 import FeaturedMovie from '@/components/featured-movie'
 import React from 'react'
-import Movies from "@/mocks/movies.json"
-import Genres from "@/mocks/genres.json"
 import Categories from '@/components/categories'
 import MoviesSection from '@/components/movies-section'
 
-const HomeContainer = ({ selectedCategory }) => {
+const HomeContainer = ({ categories = [], topRatedMovies = [], popularMovies = [], selectedCategory }) => {
 
     return (
         <>
-            <FeaturedMovie movie={Movies.results[0]} />
-            <Categories categories={Genres.genres.slice(0, 5)} />
+            <FeaturedMovie movie={popularMovies?.[0]} />
+            <Categories categories={categories.slice(0, 5)} />
             {
                 selectedCategory.movies.length > 0 &&
                 <MoviesSection
-                    title={Genres.genres.find(genre => genre.id.toString() === selectedCategory.id).name}
+                    title={categories.find(genre => genre.id.toString() === selectedCategory.id).name}
                     movies={selectedCategory.movies} />
             }
             <MoviesSection
                 title="Popular Films"
-                movies={Movies.results.slice(1, 7)} />
+                movies={topRatedMovies.slice(1, 7)} />
             <MoviesSection
                 title="Your Favorites"
-                movies={Movies.results.slice(7, 15)} />
+                movies={popularMovies.slice(1, 7)} />
         </>
     )
 }
